7f0a28bdd8015ab3cbe9f24e2ba4fdb3f2289ca5
Erwann.M <rwnnmrtn@gmail.com>
2021-08-29 12:04:54 +0200

    class Form terminé

--- include/classes/Form.php
+++ include/classes/Form.php
@@ -1,5 +1,24 @@
 <?php
 
 class Form {
-    
+
+    private $typeOfInput;
+    private $surround = 'p';
+
+    public function setInput($type) {
+        $this->typeOfInput = $type;
+    }
+
+    private function surround($tags) {
+        return "<{$this->surround}>{$tags}</{$this->surround}>";
+    }
+   
+
+    public function input($name, $class = "input") {
+        echo $this->surround("<input type='{$this->typeOfInput}' name='{$name}' class='{$class}'>");
+    }
+
+    public function submit($nameOfbtn) {
+        echo "<button type='submit' class='btn'>{$nameOfbtn}</button>";
+    }
 }
